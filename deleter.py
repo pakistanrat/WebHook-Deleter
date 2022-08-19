1bc0e0998cd3036e387f0ddbb1ddfa5e66b4c2fc
Clayze <111596797+pakistanrat@users.noreply.github.com>
2022-08-19 23:33:25 +0200

    Update deleter.py

--- deleter.py
+++ deleter.py
@@ -1,8 +1,7 @@
-import requests, os, logging, time
+import requests, os, time
 from colorama import Fore
 
 os.system('title WebHook Deleter Author: clayze#9999')
-logging.basicConfig(level=logging.INFO, format=f"{Fore.YELLOW}[{Fore.RESET}!{Fore.YELLOW}]{Fore.RESET} %(message)s{Fore.RESET}", datefmt=f"%H:%M:%S")
 
 webhook = input("Enter webhook url -> ")
 
